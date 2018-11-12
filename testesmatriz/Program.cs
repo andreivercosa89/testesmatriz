26c72a5410d8a4f26cfc1be788cf83d02a546ebf
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-12 18:43:36 -0400

    testar o maior e o maior da linha correcao

--- testesmatriz/Program.cs
+++ testesmatriz/Program.cs
@@ -6,9 +6,8 @@ namespace testesmatriz
     {
         static void Main(string[] args)
         {
-            int maior = 0;
+            int maior = 0, lmaior=0, cmaior=0;
             int[,] matriz = new int[3, 3];
-            string texto="";
             for (int linha = 0; linha < 3;linha++){
                 for (int coluna = 0; coluna < 3; coluna++)
                 {
@@ -16,12 +15,13 @@ namespace testesmatriz
                     matriz[linha,coluna] = Convert.ToInt32(Console.ReadLine());
                     if(matriz[linha,coluna]> maior){
                         maior = matriz[linha, coluna];
-                        texto = "O maior numero eh " + maior + " e esta na Linha " + linha + " e Coluna " + coluna;
+                        lmaior = linha;
+                        cmaior = coluna;
                     }
                 }
-                Console.WriteLine("na linha " + linha +": "+ texto);
+                Console.WriteLine("Na linha {0} 0 maior numero eh {1}", linha, maior);
             }
-            Console.WriteLine(texto);
+            Console.WriteLine("O maior numero eh {0} e esta na Linha {1} e na Coluna {2} ",maior,lmaior,cmaior);
         }
     }
 }
